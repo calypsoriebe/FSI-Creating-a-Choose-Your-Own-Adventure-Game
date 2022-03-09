68a33247fbc938148b5293c1d1b8f584aac6bfde
Calypso Riebe <ckriebe@gmail.com>
2022-03-08 21:32:36 -0500

    this is my adventure so far

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Your Code here
+// window.prompt('Do you head left or right?')
+let firstAnswer = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left') {
+    let secondAnswer = window.prompt('You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. Do you follow it? or continue on your path?')
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt('You come across a snoring dragon. On the other side of him, you see a shint chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?')
+}
+// this is code for all the left aswers and what follows them
+if(secondAnswer === 'follow') {
+    let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inecplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven. Do you stay or spread the word?')
+} else if(secondAnswer === 'continue') {
+    let thirdAnswer
+}
